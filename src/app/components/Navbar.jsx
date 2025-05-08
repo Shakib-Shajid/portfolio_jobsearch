df4605c2dda89530873fc8767a8802f9a76a30a1
shakib-shajid <shakibalshajid1682@gmail.com>
2025-05-09 02:17:41 +0600

    navbar login and logout added

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -39,7 +39,10 @@ const Navbar = () => {
                         <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                             <li><Link href="/">Home</Link></li>
                             <li><Link href="/jobs">Jobs</Link></li>
-                            <li><Link href="/hiring">Hiring</Link></li>
+                            {
+                                session?.status === "authenticated" &&
+                                <li><Link href="/hiring">Hiring</Link></li>
+                            }
                         </ul>
                     </div>
                     <Link href="/" className="btn btn-ghost text-lg md:text-xl -ml-5 lg:ml-0">Portfolio & Job</Link>
@@ -48,42 +51,60 @@ const Navbar = () => {
                 <div className="navbar-center hidden lg:flex gap-5">
                     <Link href="/">Home</Link>
                     <Link href="/jobs">Jobs</Link>
-                    <Link href="/hiring">Hiring</Link>
+                    {
+                        session?.status === "authenticated" && <Link href="/hiring">Hiring</Link>
+                    }
                 </div>
 
                 <div className="flex gap-2 navbar-end relative">
-                    <input
-                        type="text"
-                        placeholder="Search Job"
-                        className="input input-bordered w-36 md:w-auto rounded-full text-xs md:text-sm"
-                        value={search}
-                        onChange={(e) => setSearch(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                    />
-                    <button
-                        className="absolute z-10 right-14 text-xs btn btn-xs btn-success rounded-full shadow-none"
-                        onClick={handleSearch}
-                    >
-                        Search
-                    </button>
-
-                    {/* 
-                    <div className="dropdown dropdown-end">
-                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                            <div className="w-10 rounded-full">
-                                <img alt="Profile" src="https://rb.gy/c28gf3" />
-                            </div>
+                    {
+                        session?.status === "authenticated" &&
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Search Job"
+                                className="input input-bordered w-36 md:w-auto rounded-full text-xs md:text-sm"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                            />
+                            <button
+                                className="absolute z-10 top-2 right-14 text-xs btn btn-xs btn-success rounded-full shadow-none"
+                                onClick={handleSearch}
+                            >
+                                Search
+                            </button>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                            <li><Link href="/profile">Profile</Link></li>
-                            <li><Link href="/">Settings</Link></li>
-                            <li><Link href="/">Logout</Link></li>
-                        </ul>
+                    }
 
-                        
-                    </div> */}
+                    {
+                        session?.status === 'loading' &&
+                        <h6>Loading....</h6>
+                    }
 
-               
+
+                    {session?.status === 'unauthenticated' &&
+                        <Link href="/login" className="btn btn-info text-white rounded-full ">Login</Link>
+                    }
+                    {session?.status === 'authenticated' &&
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img alt="Profile" src="https://rb.gy/c28gf3" />
+                                </div>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                <li><Link href="/profile">Profile</Link></li>
+                                <li><Link href="/">Settings</Link></li>
+                                <li><button onClick={() => signOut()}>Logout</button></li>
+
+                            </ul>
+
+
+                        </div>
+                    }
+
+
                 </div>
             </div>
         </div>
